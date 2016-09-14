9a0146153a7da1409f8495fd2c50e675686d204f
Gerard Roche <gerardroche@users.noreply.github.com>
2016-09-14 10:36:17 +0100

    Pedal to the :metal:

--- test/MySQLTest.php
+++ test/MySQLTest.php
@@ -6,25 +6,51 @@ use PDO;
 
 class MySQLTest extends \PHPUnit_Framework_TestCase
 {
-    public function testMysql()
+    protected static $pdo;
+
+    protected function setUp()
     {
-        $pdo = new PDO('mysql:charset=utf8', 'root', '');
+        if (self::$pdo === null) {
+            self::$pdo = new PDO(
+                'mysql:charset=utf8',
+                DBUNIT_USERNAME,
+                DBUNIT_PASSWORD
+            );
 
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        $pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            self::$pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
+            self::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        }
+    }
 
-        var_dump($pdo->query('
-            SELECT
-                @@GLOBAL.time_zone,
-                @@SESSION.time_zone,
-                @@time_zone,
-                @@GLOBAL.sql_mode,
-                @@SESSION.sql_mode,
-                @@sql_mode
-        ')->fetchAll());
+    public function testTimeZone()
+    {
+        $this->assertSame('SYSTEM', self::$pdo->query('SELECT @@time_zone')->fetchColumn());
+    }
 
-        $this->assertTrue(true);
+    public function testGlobalTimeZone()
+    {
+        $this->assertSame('SYSTEM', self::$pdo->query('SELECT @@GLOBAL.time_zone')->fetchColumn());
+    }
+
+    public function testSessionTimeZone()
+    {
+        $this->assertSame('SYSTEM', self::$pdo->query('SELECT @@SESSION.time_zone')->fetchColumn());
+    }
+
+    public function testSQLMode()
+    {
+        $this->assertSame('', self::$pdo->query('SELECT @@sql_mode')->fetchColumn());
+    }
+
+    public function testGlobalSQLMode()
+    {
+        $this->assertSame('', self::$pdo->query('SELECT @@GLOBAL.sql_mode')->fetchColumn());
+    }
+
+    public function testSessionSQLMode()
+    {
+        $this->assertSame('', self::$pdo->query('SELECT @@SESSION.sql_mode')->fetchColumn());
     }
 }
